6915ace601f93fa510fc5695974910cdfd39541b
Jose Lazaro Izquierdo <joselaz88@outlook.com>
2024-09-11 22:23:06 -0600

    se finalizo esta tarea

--- circulo.py
+++ circulo.py
@@ -5,6 +5,9 @@ class Circulo:
     def __init__(self, radio):
         self.radio = radio
 
-    def mostrar_info(self):
+    def calcular_area(self):
+        print("El radio es de: ", self.radio)
         print("El area es: ", 3.1416*self.radio**2, "m^2")
+
+    def calcular_perimetro(self):
         print("El perimetro es: ", 2*self.radio*3.1416, "m")
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from circulo import Circulo
 
 circulo_uno = Circulo(2.7)
-circulo_uno.mostrar_info()
+circulo_uno.calcular_area()
+circulo_uno.calcular_perimetro()
 
 print("*************************")
 
 circulo_dos = Circulo(6.5)
-circulo_dos.mostrar_info()
+circulo_dos.calcular_area()
+circulo_dos.calcular_perimetro()
 
 print("*************************")
 
 circulo_tres = Circulo(12.3)
-circulo_tres.mostrar_info()
+circulo_tres.calcular_area()
+circulo_tres.calcular_perimetro()
