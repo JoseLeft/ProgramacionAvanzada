39b8ce1267027d41d02f5fce7b72c832a5d568cd
Jose Lazaro Izquierdo <joselaz88@outlook.com>
2024-10-18 00:10:59 -0600

    Se termino la tarea

--- estudiantes/estudiante.py
+++ estudiantes/estudiante.py
@@ -1,15 +1,18 @@
 from datetime import datetime
+from usuario.usuario import Usuario
+from usuario.utils.roles import Roles
 
-class Estudiante:
-    numero_control: str
-    nombre: str
-    apellido: str
+class Estudiante(Usuario):
     curp: str
     fecha_nacimiento: datetime
+    
 
-    def __init__(self, numero_control:str, nombre:str, apellido:str, curp:str, fecha_nacimiento:datetime):
-        self.numero_control = numero_control
-        self.nombre = nombre
-        self.apellido = apellido
-        self.curp = curp
-        self.fecha_nacimiento = fecha_nacimiento
+    def __init__(self, numero_control: str, nombre: str, apellido: str, curp: str, fecha_nacimiento: datetime, contraseña: str):
+        super().__init__(nuemro_de_control=numero_control, nombre=nombre, apellido=apellido, contraseña=contraseña, rol=Roles.ESTUDIANTE)
+        self.curp=curp
+        self.fecha_nacimiento=fecha_nacimiento
+
+    def mostrar_info_estudiante(self):
+        nombre_completo=f"{self.nombre} {self.apellido}"
+        info=f"\n-Numero de control: {self.numero_control}\n-Nombre completo: {nombre_completo}\n-CURP: {self.curp}\n-Fecha de nacimiento: {self.fecha_nacimiento}\n-Rol: {self.rol.value}"
+        return info
--- maestros/maestro.py
+++ maestros/maestro.py
@@ -1,17 +1,18 @@
-from datetime import datetime
+from usuario.usuario import Usuario
+from usuario.utils.roles import Roles
 
-class Maestro:
-    numero_control: str
-    nombre: str
-    apellido: str
-    ano_nacimiento: datetime
+class Maestro(Usuario):
     rfc: str
     sueldo: float
+    año_nacimiento: int
 
-    def __init__(self, numero_control: str, nombre:str, apellido:str, ano_nacimiento: datetime, rfc:str, sueldo:float):
-        self.numero_control = numero_control 
-        self.nombre = nombre
-        self.apellido = apellido
-        self.ano_nacimiento = ano_nacimiento
-        self.rfc = rfc
-        self.sueldo = sueldo
+    def __init__(self, nombre: str, apellido: str, rfc: str, sueldo: float, numero_control: str, año: int, contraseña: str):
+        super().__init__(nuemro_de_control=numero_control, nombre=nombre, apellido=apellido, contraseña=contraseña, rol=Roles.MAESTRO)
+        self.rfc=rfc
+        self.sueldo=sueldo
+        self.año_nacimiento=año
+
+    def mostrar_info_maestro(self):
+        nombre_completo=f"{self.nombre} {self.apellido}"
+        info=f"\n-Numero de control: {self.numero_control}\n-Nombre completo: {nombre_completo}\n-RFC: {self.rfc}\n-Sueldo: {self.sueldo}\n-Rol{self.rol.value}"
+        return info
